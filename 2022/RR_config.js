4efab1766c83b2ad18ff58229d4b1d2bec4396db
empirebuilder1 <xkernel64@gmail.com>
2022-03-25 14:00:33 -0700

    Update RR_config.js
    reenable

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -16,7 +16,7 @@ var config_data = `
         "type":"event",
         "defaultValue":"2022orsal",
         "required":"true",
-        "disabled":"true"
+        "disabled":"false"
       },
       "Match Level":{
         "code":"l",
